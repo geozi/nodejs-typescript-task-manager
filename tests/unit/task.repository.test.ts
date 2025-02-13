b94cd2dd4376f653527ca9db349acd4bb77e04da
geozi <yorgosaz@gmail.com>
2025-02-13 13:22:59 +0200

    Rewritten: task.repository unit tests.

--- tests/unit/task.repository.test.ts
+++ tests/unit/task.repository.test.ts
@@ -1,81 +1,152 @@
 /**
  * Task repository unit tests.
  */
-import mongoose from "mongoose";
 import testInput from "../testInput";
 import assert from "assert";
 import sinon from "sinon";
 import Task from "../../src/domain/models/task.model";
 import { ITask } from "../../src/domain/interfaces/iTask.interface";
-import { addTask } from "../../src/persistence/task.repository";
+import {
+  addTask,
+  deleteTask,
+  getTaskBySubject,
+  getTasksByStatus,
+  getTasksByUsername,
+  updateTask,
+} from "../../src/persistence/task.repository";
+import { Types } from "mongoose";
 
-describe("Task repository unit test", () => {
+describe.only("Task repository unit tests", () => {
   let newTask: ITask;
+  const mockTask = new Task();
+  const mockTasks = [new Task(), new Task()];
+  const mockId = new Types.ObjectId("67adcdfd4265b2332f658fc3");
 
-  describe("addTask() test(s)", () => {
-    describe("Promise<ITask> resolves", () => {
-      beforeEach(() => {
-        sinon.stub(Task.prototype, "save").resolves({});
-      });
-
-      afterEach(() => {
-        sinon.restore();
-      });
-
-      it("task added", () => {
-        newTask = new Task(testInput.validTaskInput);
-        assert.doesNotReject(async () => {
-          await addTask(newTask);
-        });
-      });
+  describe("getTasksByStatus()", () => {
+    beforeEach(() => {
+      sinon.restore();
     });
 
-    describe("Promise<ITask> rejects", () => {
-      beforeEach(() => {
-        sinon
-          .stub(Task.prototype, "save")
-          .rejects(new mongoose.Error.ValidationError());
-      });
+    it("Promise resolves to Task object array", async () => {
+      sinon.stub(Task, "find").resolves(mockTasks);
+      const foundTasks = await getTasksByStatus(
+        testInput.validTaskInput.status
+      );
 
-      afterEach(() => {
-        sinon.restore();
-      });
+      assert(foundTasks instanceof Array);
+      assert.strictEqual(foundTasks.length, 2);
+    });
 
-      it("has too short subject", () => {
-        newTask = new Task(testInput.validTaskInput);
-        newTask.subject = testInput.invalidTaskInputs.TOO_SHORT_SUBJECT;
+    it("Promise resolves to empty array", async () => {
+      sinon.stub(Task, "find").resolves([]);
+      const foundTasks = await getTasksByStatus(
+        testInput.validTaskInput.status
+      );
 
-        assert.rejects(async () => {
-          await addTask(newTask);
-        }, mongoose.Error.ValidationError);
-      });
+      assert(foundTasks instanceof Array);
+      assert.strictEqual(foundTasks.length, 0);
+    });
+  });
 
-      it("has too long subject", () => {
-        newTask = new Task(testInput.validTaskInput);
-        newTask.subject = testInput.invalidTaskInputs.TOO_LONG_SUBJECT;
+  describe("getTasksByUsername()", () => {
+    beforeEach(() => {
+      sinon.restore();
+    });
 
-        assert.rejects(async () => {
-          await addTask(newTask);
-        }, mongoose.Error.ValidationError);
-      });
+    it("Promise resolves to Task object array", async () => {
+      sinon.stub(Task, "find").resolves(mockTasks);
+      const foundTasks = await getTasksByUsername(
+        testInput.validTaskInput.username
+      );
 
-      it("has too long description", () => {
-        newTask = new Task(testInput.validTaskInput);
-        newTask.description = testInput.invalidTaskInputs.TOO_LONG_DESCRIPTION;
+      assert(foundTasks instanceof Array);
+      assert.strictEqual(foundTasks.length, 2);
+    });
 
-        assert.rejects(async () => {
-          await addTask(newTask);
-        }, mongoose.Error.ValidationError);
-      });
+    it("Promise resolves to empty array", async () => {
+      sinon.stub(Task, "find").resolves([]);
+      const foundTasks = await getTasksByUsername(
+        testInput.validTaskInput.username
+      );
 
-      it("has invalid status", () => {
-        newTask = new Task(testInput.validTaskInput);
-        newTask.status = testInput.invalidTaskInputs.INVALID_STATUS;
+      assert(foundTasks instanceof Array);
+      assert.strictEqual(foundTasks.length, 0);
+    });
+  });
 
-        assert.rejects(async () => {
-          await addTask(newTask);
-        }, mongoose.Error.ValidationError);
-      });
+  describe("getTasksBySubject()", () => {
+    beforeEach(() => {
+      sinon.restore();
+    });
+
+    it("Promise resolves to Task object", async () => {
+      sinon.stub(Task, "findOne").resolves(mockTask);
+      const foundTask = await getTaskBySubject(
+        testInput.validTaskInput.subject
+      );
+      assert(foundTask instanceof Task);
+    });
+
+    it("Promise resolves to null", async () => {
+      sinon.stub(Task, "findOne").resolves(null);
+      const foundTask = await getTaskBySubject(
+        testInput.validTaskInput.subject
+      );
+      assert.strictEqual(foundTask, null);
+    });
+  });
+
+  describe("addTask()", () => {
+    beforeEach(() => {
+      sinon.restore();
+      newTask = new Task();
+    });
+
+    it("Promise resolves to Task object", async () => {
+      sinon.stub(Task.prototype, "save").resolves(mockTask);
+      const savedTask = await addTask(newTask);
+
+      assert(savedTask instanceof Task);
+    });
+  });
+
+  describe("updateTask()", () => {
+    beforeEach(() => {
+      sinon.restore();
+    });
+
+    it("Promise resolves to Task object", async () => {
+      sinon.stub(Task, "findByIdAndUpdate").resolves(mockTask);
+      const updatedTask = await updateTask(mockId, testInput.validTaskInput);
+
+      assert(updatedTask instanceof Task);
+    });
+
+    it("Promise resolves to null", async () => {
+      sinon.stub(Task, "findByIdAndUpdate").resolves(null);
+      const updatedTask = await updateTask(mockId, testInput.validTaskInput);
+
+      assert.strictEqual(updatedTask, null);
+    });
+  });
+
+  describe("deleteTask()", () => {
+    beforeEach(() => {
+      sinon.restore();
+    });
+
+    it("Promise resolves to Task object", async () => {
+      sinon.stub(Task, "findByIdAndDelete").resolves(mockTask);
+      const deletedTask = await deleteTask(mockId);
+
+      assert(deletedTask instanceof Task);
+    });
+
+    it("Promise resolves to null", async () => {
+      sinon.stub(Task, "findByIdAndDelete").resolves(null);
+      const deletedTask = await deleteTask(mockId);
+
+      assert.strictEqual(deletedTask, null);
     });
   });
 });
