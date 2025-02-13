ca7b2e56b3dd8e6fb23593f413a36ab834c26d62
geozi <yorgosaz@gmail.com>
2025-02-13 17:04:21 +0200

    Added: chai and chai-as-promised;  Rewritten: task.service unit tests.

--- tests/unit/task.service.test.ts
+++ tests/unit/task.service.test.ts
@@ -4,7 +4,6 @@
 import Task from "../../src/domain/models/task.model";
 import { ITask } from "../../src/domain/interfaces/iTask.interface";
 import { ITaskUpdate } from "../../src/presentation/interfaces/iTaskUpdate.interface";
-import assert from "assert";
 import testInput from "../testInput";
 import sinon from "sinon";
 import {
@@ -18,121 +17,138 @@ import {
 import * as taskRepository from "../../src/persistence/task.repository";
 import { NotFoundError } from "../../src/service/errors/notFound.error";
 import { ServerError } from "../../src/persistence/errors/server.error";
+import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
 
 describe("Task service unit test", () => {
-  const validTask = new Task(testInput.validTaskInput);
-  const mockTasks: ITask[] = [];
-  const mockTask = new Task();
   const mockUpdateDataObj: ITaskUpdate = {
     id: "678f6f5feeb9f5507709b24e",
   };
   const mockId = "678f7e88196eec7b9d3d549a";
   let methodStub: sinon.SinonStub;
 
-  describe("Promise rejects", () => {
-    describe("retrieveTasksByStatus()", () => {
-      beforeEach(() => {
-        methodStub = sinon.stub(taskRepository, "getTasksByStatus");
-      });
-
-      afterEach(() => {
-        methodStub.restore();
-      });
-
-      it("server error", async () => {
-        methodStub.rejects();
-        await assert.rejects(async () => {
-          await retrieveTasksByStatus(validTask.status);
-        }, ServerError);
-      });
-
-      it("tasks not found", async () => {
-        methodStub.resolves(mockTasks);
-        await assert.rejects(async () => {
-          await retrieveTasksByStatus(validTask.status);
-        }, NotFoundError);
-      });
+  describe("retrieveTasksByStatus()", () => {
+    beforeEach(() => {
+      methodStub = sinon.stub(taskRepository, "getTasksByStatus");
     });
 
-    describe("retrieveTasksByUsername()", () => {
-      beforeEach(() => {
-        methodStub = sinon.stub(taskRepository, "getTasksByUsername");
-      });
-
-      afterEach(() => {
-        methodStub.restore();
-      });
-
-      it("server error", async () => {
-        methodStub.rejects();
-        await assert.rejects(async () => {
-          await retrieveTasksByUsername(validTask.username);
-        }, ServerError);
-      });
-
-      it("tasks not found", async () => {
-        methodStub.resolves(mockTasks);
-        await assert.rejects(async () => {
-          await retrieveTasksByUsername(validTask.username);
-        }, NotFoundError);
-      });
+    afterEach(() => {
+      methodStub.restore();
     });
 
-    describe("retrieveTaskBySubject()", () => {
-      beforeEach(() => {
-        methodStub = sinon.stub(taskRepository, "getTaskBySubject");
-      });
+    it("server error", async () => {
+      methodStub.rejects();
 
-      afterEach(() => {
-        methodStub.restore();
-      });
-
-      it("server error", async () => {
-        methodStub.rejects();
-        await assert.rejects(async () => {
-          await retrieveTaskBySubject(validTask.subject);
-        });
-      });
-
-      it("task not found", async () => {
-        methodStub.resolves(null);
-        await assert.rejects(async () => {
-          await retrieveTaskBySubject(validTask.subject);
-        }, NotFoundError);
-      });
+      await chai
+        .expect(retrieveTasksByStatus(testInput.validTaskInput.status))
+        .to.be.rejectedWith(ServerError);
     });
 
-    describe("createTaskRecord()", () => {
-      it("server error", async () => {
-        sinon.stub(taskRepository, "addTask").rejects();
-        await assert.rejects(async () => {
-          await createTaskRecord(mockTask);
-        }, ServerError);
-      });
+    it("tasks not found", async () => {
+      methodStub.resolves([]);
+
+      await chai
+        .expect(retrieveTasksByStatus(testInput.validTaskInput.status))
+        .to.be.rejectedWith(NotFoundError);
+    });
+  });
+
+  describe("retrieveTasksByUsername()", () => {
+    beforeEach(() => {
+      methodStub = sinon.stub(taskRepository, "getTasksByUsername");
     });
 
-    describe("updateTaskRecord()", () => {
-      beforeEach(() => {
-        methodStub = sinon.stub(taskRepository, "updateTask");
-      });
+    afterEach(() => {
+      methodStub.restore();
+    });
 
-      afterEach(() => {
-        methodStub.restore();
-      });
+    it("server error", async () => {
+      methodStub.rejects();
 
-      it("server error", async () => {
-        methodStub.rejects();
-        await assert.rejects(async () => {
-          await updateTaskRecord(mockUpdateDataObj);
-        }, ServerError);
-      });
+      await chai
+        .expect(retrieveTasksByUsername(testInput.validTaskInput.username))
+        .to.be.rejectedWith(ServerError);
+    });
 
-      it("task not found()", async () => {
-        methodStub.resolves(null);
-        await assert.rejects(async () => {
-          await updateTaskRecord(mockUpdateDataObj);
-        }, NotFoundError);
-      });
+    it("tasks not found", async () => {
+      methodStub.resolves([]);
+
+      await chai
+        .expect(retrieveTasksByUsername(testInput.validTaskInput.username))
+        .to.be.rejectedWith(NotFoundError);
+    });
+  });
+
+  describe("retrieveTaskBySubject()", () => {
+    beforeEach(() => {
+      methodStub = sinon.stub(taskRepository, "getTaskBySubject");
+    });
+
+    afterEach(() => {
+      methodStub.restore();
+    });
+
+    it("server error", async () => {
+      methodStub.rejects();
+
+      await chai
+        .expect(retrieveTaskBySubject(testInput.validTaskInput.subject))
+        .to.be.rejectedWith(ServerError);
+    });
+
+    it("task not found", async () => {
+      methodStub.resolves(null);
+
+      await chai
+        .expect(retrieveTaskBySubject(testInput.validTaskInput.subject))
+        .to.be.rejectedWith(NotFoundError);
+    });
+  });
+
+  describe("createTaskRecord()", () => {
+    let newTask: ITask;
+    beforeEach(() => {
+      methodStub = sinon.stub(taskRepository, "addTask");
+      newTask = new Task();
+    });
+
+    afterEach(() => {
+      methodStub.restore();
+    });
+
+    it("server error", async () => {
+      methodStub.rejects();
+
+      await chai
+        .expect(createTaskRecord(newTask))
+        .to.be.rejectedWith(ServerError);
+    });
+  });
+
+  describe("updateTaskRecord()", () => {
+    beforeEach(() => {
+      methodStub = sinon.stub(taskRepository, "updateTask");
+    });
+
+    afterEach(() => {
+      methodStub.restore();
+    });
+
+    it("server error", async () => {
+      methodStub.rejects();
+
+      await chai
+        .expect(updateTaskRecord(mockUpdateDataObj))
+        .to.be.rejectedWith(ServerError);
+    });
+
+    it("not found", async () => {
+      methodStub.resolves(null);
+
+      await chai
+        .expect(updateTaskRecord(mockUpdateDataObj))
+        .to.be.rejectedWith(NotFoundError);
     });
   });
 
@@ -147,16 +163,18 @@ describe("Task service unit test", () => {
 
     it("server error", async () => {
       methodStub.rejects();
-      await assert.rejects(async () => {
-        await deleteTaskRecord(mockId);
-      }, ServerError);
+
+      await chai
+        .expect(deleteTaskRecord(mockId))
+        .to.be.rejectedWith(ServerError);
     });
 
-    it("task not found", async () => {
+    it("not found", async () => {
       methodStub.resolves(null);
-      await assert.rejects(async () => {
-        await deleteTaskRecord(mockId);
-      }, NotFoundError);
+
+      await chai
+        .expect(deleteTaskRecord(mockId))
+        .to.be.rejectedWith(NotFoundError);
     });
   });
 });
